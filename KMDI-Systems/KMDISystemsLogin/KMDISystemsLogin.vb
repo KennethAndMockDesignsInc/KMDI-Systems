f2d7b6900d910c6d1abea17df05dc78f28f267ad
Russell222 <26921122+Russell222@users.noreply.github.com>
2018-06-30 08:49:13 +0800

    Log in

--- KMDI-Systems/KMDISystemsLogin/KMDISystemsLogin.vb
+++ KMDI-Systems/KMDISystemsLogin/KMDISystemsLogin.vb
@@ -1,6 +1,24 @@
 ﻿Public Class KMDISystemsLogin
+    Dim KMDISystems_UserName As String
+    Dim KMDISystems_Password As String
+
     Private Sub KMDISystemsLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ConnectionTypeCbox.SelectedIndex = 0
         MaximizeBox = False
+    End Sub
+
+    Private Sub PasswordTbox_KeyDown(sender As Object, e As KeyEventArgs) Handles PasswordTbox.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            KMDISystems_Login(KMDISystems_UserName,
+                              KMDISystems_Password)
+        End If
+    End Sub
+
+    Private Sub UserNameTbox_TextChanged(sender As Object, e As EventArgs) Handles UserNameTbox.TextChanged
+        KMDISystems_UserName = UserNameTbox.Text
+    End Sub
+
+    Private Sub PasswordTbox_TextChanged(sender As Object, e As EventArgs) Handles PasswordTbox.TextChanged
+        KMDISystems_Password = PasswordTbox.Text
     End Sub
 End Class
--- KMDI-Systems/KMDISystemsLogin/LoginModule.vb
+++ KMDI-Systems/KMDISystemsLogin/LoginModule.vb
@@ -1,3 +1,81 @@
-﻿Module LoginModule
+﻿Imports System.IO
+Imports System.Text
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Security.Cryptography
+Imports System.Windows.Forms.DataVisualization.Charting
 
+Module LoginModule
+    Dim LocalAccess As String = "192.168.1.21,49107"
+    Dim DBName As String = "KMDIDATA"
+    Dim DBUserName As String = "kmdiadmin"
+    Dim DBPassword As String = "kmdiadmin"
+    Public sqlConnection As New SqlConnection With {.ConnectionString = "Data Source='" & LocalAccess & "';Network Library=DBMSSOCN;Initial Catalog='" & DBName & "';User ID='" & DBUserName & "';Password='" & DBPassword & "';"}
+    Public sqlCommand As SqlCommand
+    Public sqlDataAdapter As SqlDataAdapter
+    Public Read As SqlDataReader
+
+    Public Sub KMDISystems_Login(ByVal UserName As String,
+                                 ByVal Password As String)
+        Try
+            sqlConnection.Close()
+            sqlConnection.Open()
+            Dim DataSet As New DataSet
+            Dim BindingSource As New BindingSource
+            DataSet.Clear()
+            Dim Query As String = "Select *
+                                   From KMDI_ACCT_TB
+                                   Where [username] COLLATE Latin1_General_CS_AS = '" & UserName & "' AND [password] COLLATE Latin1_General_CS_AS= '" & Encrypt(Password) & "'"
+            sqlCommand = New SqlCommand(Query, sqlConnection)
+            'sqlDataAdapter.SelectCommand = sqlCommand
+            'sqlDataAdapter.Fill(DataSet, "KMDI_ACCT_TB")
+            'BindingSource.DataSource = DataSet
+            'BindingSource.DataMember = "KMDI_ACCT_TB"
+
+            Read = sqlCommand.ExecuteReader
+            If Read.HasRows = True Then
+                ManageAccounts.Show()
+            Else
+                MessageBox.Show("Failed")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+    End Sub
+
+    Public Function Encrypt(clearText As String) As String
+
+        Dim EncryptionKey As String = "MAKV2SPBNI99212"
+
+        Dim clearBytes As Byte() = Encoding.Unicode.GetBytes(clearText)
+
+        Using encryptor As Aes = Aes.Create()
+
+            Dim pdb As New Rfc2898DeriveBytes(EncryptionKey, New Byte() {&H49, &H76, &H61, &H6E, &H20, &H4D, &H65, &H64, &H76, &H65, &H64, &H65, &H76})
+
+            encryptor.Key = pdb.GetBytes(32)
+
+            encryptor.IV = pdb.GetBytes(16)
+
+            Using ms As New MemoryStream()
+
+                Using cs As New CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write)
+
+                    cs.Write(clearBytes, 0, clearBytes.Length)
+
+                    cs.Close()
+
+                End Using
+
+                clearText = Convert.ToBase64String(ms.ToArray())
+
+            End Using
+
+        End Using
+
+        Return clearText
+
+    End Function
 End Module
